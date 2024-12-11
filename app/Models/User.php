b9d81a9f15c77fb18fae871180efd4dbbe65bbdc
abdoalielbaz <abdoalielbaz@gmail.com>
2024-12-11 07:06:11 +0200

    add-budget-resource

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function budgets()
+    {
+        return $this->belongsToMany(Budget::class);
+    }
 }
